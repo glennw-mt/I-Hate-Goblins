620258c8c294740b7610a5628e208fe9fb100afe
Glenn Wallace <glennw-coding@proton.me>
2025-11-29 20:55:14 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# https://creativecommons.org/publicdomain/zero/1.0/
+"""PyInstaller main script example."""
+
+from pathlib import Path
+
+import tcod.console
+import tcod.context
+import tcod.event
+import tcod.tileset
+
+WIDTH, HEIGHT = 80, 60
+
+BASE_DIR = Path(__file__).parent
+"""The directory of this script."""
+
+FONT_PATH = BASE_DIR / "data/terminal8x8_gs_ro.png"
+
+
+def main() -> None:
+    """Entry point function."""
+    tileset = tcod.tileset.load_tilesheet(FONT_PATH, 16, 16, tcod.tileset.CHARMAP_CP437)
+    with tcod.context.new(columns=WIDTH, rows=HEIGHT, tileset=tileset) as context:
+        while True:
+            console = tcod.console.Console(WIDTH, HEIGHT)
+            console.print(0, 0, "Hello World")
+            context.present(console)
+            for event in tcod.event.wait():
+                if isinstance(event, tcod.event.Quit):
+                    raise SystemExit
+
+
+if __name__ == "__main__":
+    main()
